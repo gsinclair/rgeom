446ad404ab10587416332f85ea6208997fb8288f
Gavin Sinclair <gsinclair@gmail.com>
2009-11-21 16:41:16 +1100

    html-demo 02/04 -- Pythagoras's Theorem again
    Using semicircles on each side of the triangle.

---
+++ html-demo/02-showcase/04/in.rb
@@ -0,0 +1,5 @@
+triangle :ABC, :right_angle => :A, :base => 4, :height => 3
+semicircle :diameter => :BA
+semicircle :diameter => :CB
+semicircle :diameter => :AC
+square :A___, :side => 0.4    # right-angle mark
