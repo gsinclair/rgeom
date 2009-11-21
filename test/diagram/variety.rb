4ba69c9da7dc08b222e05955efc233d492872d85
Gavin Sinclair <gsinclair@gmail.com>
2009-11-21 16:38:52 +1100

    test/diagram/variety.rb: basic test of diagrams
    Can't programatically check correctness of diagrams, but we can
    at least ensure the diagram code is run in a unit test to ensure
    there are no exceptions or syntax errors.

---
+++ test/diagram/variety.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require 'rgeom'
+include RGeom::Assertions
+include RGeom
+
+  # Simple diagrams.  We don't confirm the correctness of them, just that
+  # they can be created and therefore the relevant diagram-producing code is
+  # at least somewhat correct.
+class TestDiagramVariety < Test::Unit::TestCase
+
+  FILENAME = "out/variety.png"
+
+  TESTS = []
+  TESTS << { :desc => "Triangle",   :code => "triangle" }
+  TESTS << { :desc => "Circle",     :code => "circle" }
+  TESTS << { :desc => "Arc",        :code => "arc(:angles => [3,189])" }
+  TESTS << { :desc => "Semicircle", :code => "semicircle" }
+
+  TESTS.each do |test|
+    description = test[:desc].downcase
+    define_method("test_#{description}") {
+      filename = "out/variety-#{description}.png"
+      FileUtils.rm_f filename
+      RGeom::Register.instance.clear!
+      debug test[:desc]
+      debug test[:code]
+      eval test[:code]
+      render filename
+      assert File.file?(filename)
+    }
+  end
+
+  debug "TestDiagramVariety has the following methods:"
+  debug (TestDiagramVariety.instance_methods - Object.instance_methods).
+    sort.join(" ").indent(3)
+
+end  # class TestDiagramVariety
