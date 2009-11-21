c0994f4b9a9ad7aef56b3969265e5a9bbea37ff9
Gavin Sinclair <gsinclair@gmail.com>
2009-11-22 09:40:11 +1100

    Removed TODO item about register
    And fixed typo in Example 2.05.

--- html-demo/02-showcase/05/in.rb
+++ html-demo/02-showcase/05/in.rb
@@ -1,7 +1,7 @@
 circle :radius => 3
 
 # Create a series of arcs starting at the given angles and continuing
-# for 40 degrees.
+# for 60 degrees.
 [-60, 60, 180].each do |angle|
   arc1 = arc :radius => 4,  :angles => [angle, angle+60]
   arc2 = arc :radius => 12, :angles => [angle, angle+60]
