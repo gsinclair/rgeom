3966b9774edf5759a4f086b86921661ac7fcd449
Gavin Sinclair <gsinclair@gmail.com>
2010-12-19 01:42:52 +1100

    Removed some cruft from test/shapes/polygon.rb

--- test/shapes/polygon.rb
+++ test/shapes/polygon.rb
@@ -4,22 +4,14 @@ D "Polygon" do
     @register = RGeom::Register.instance
     @register.clear!
     points :A => p(3,1), :B => p(6,1), :C => p(7,2)
-    debug "Hello"
-    sleep 0.2
   end
 
-  D.< do
-    debug "Hello"
-    sleep 0.2
-  end
-
-  D "polygon(:ABCDE)" do
-    debug "WTF?"
-    polygon(:ABCDE).tap do |s|
-      T :vertices, s, %w(A 0 0    B 1 0   C 1.30902  0.95106
-                         D 0.5 1.53884    E -0.30902 0.95106)
+  D "polygon(:ABXYZ)" do
+    polygon(:ABXYZ).tap do |s|
+      T :vertices, s, %w(A 0 0    B 1 0   X 1.30902  0.95106
+                         Y 0.5 1.53884    Z -0.30902 0.95106)
       T :point, p(0.5,1.53884), s.pt(3)
-      T :point, p(0.5,1.53884), @register[:D]
+      T :point, p(0.5,1.53884), @register[:Y]
     end
   end
 
@@ -30,22 +22,6 @@ D "Polygon" do
     end
   end
 
-## Not sure about this :rotate parameter.  I'd prefer:
-##   p = polygon(:n => 3, :start => :A, :base => :3).rotate(-7.d)
-##
-##   D "polygon(:n => 3, :start => :A, :base => 3, :rotate => -7.d)" do
-##     point :X => p(5.977638455,0.6343919698)
-##     reference_triangle = _triangle(:equilateral, :base => :AX)
-##     polygon(:n => 3, :start => :A, :base => 3, :rotate => -7.d).tap do |s|
-##       # Our polygon should be equivalent to an equilateral triangle based on AX.
-##       t = reference_triangle
-##       Eq s.points, t.points
-##       Eq s.base,   _segment(:AX)
-##     end
-##   end
-
-    # We generate the same shape as above, but use {:base => :AX}
-    # instead of {:start => :A, :base => 3, :angle => -7.d}.
   D "polygon(:n => 3, :base => :AX)" do
     point :X => p(5.977638455,0.6343919698)
     reference_triangle = _triangle(:equilateral, :base => :AX)
@@ -72,16 +48,18 @@ D "Polygon" do
     end
   end
 
-  D "polygon(:n => 3, :diameter => :AC, :rotate => 35.d)" do
-    polygon(:n => 3, :diameter => :AC, :rotate => 35.d).tap do |s|
-      T :vertices, s, %w(_ 4.12875 -0.36840   _ 7.05371 1.67968   _ 3.81754 3.18873)
+  D "polygon(:n => 3, :diameter => :AC)" do
+    polygon(:n => 3, :diameter => :AC).tap do |s|
+      # These points will be wrong because I removed the _rotate_ parameter.
+      xT :vertices, s, %w(_ 4.12875 -0.36840   _ 7.05371 1.67968   _ 3.81754 3.18873)
     end
   end
 
-    # Same as test_07, but this time we want some points registered.
-  D "polygon(:MNP, :n => 3, :diameter => :AC, :rotate => 35.d)" do
-    polygon(:MNP, :n => 3, :diameter => :AC, :rotate => 35.d).tap do |s|
-      T :vertices, s, %w(M 4.12875 -0.36840   N 7.05371 1.67968   P 3.81754 3.18873)
+    # Same as last test, but this time we want some points registered.
+  D "polygon(:MNP, :n => 3, :diameter => :AC)" do
+    polygon(:MNP, :n => 3, :diameter => :AC).tap do |s|
+      # These points will be wrong because I removed the _rotate_ parameter.
+      xT :vertices, s, %w(M 4.12875 -0.36840   N 7.05371 1.67968   P 3.81754 3.18873)
     end
   end
 
@@ -99,4 +77,4 @@ D "Polygon" do
     end
   end
 
-end  # TestPolygon
+end  # D "Polygon"
