286d5dce187f6a7c072dc20bda8394a4070bc48d
Gavin Sinclair <gsinclair@gmail.com>
2010-12-23 14:41:28 +1100

    Refactored test/diagrams.rb
     * Easier to add new diagram tests now.

--- test/diagrams.rb
+++ test/diagrams.rb
@@ -3,35 +3,44 @@ require 'fileutils'
 
 D "Diagrams" do
 
+  D.<< do
+    Dir['out/*.png'].each do |p|
+      FileUtils.rm_f(p)
+    end
+  end
+
   D.< do
     RGeom::Register.instance.clear!
   end
 
-  D "circles" do
-    FileUtils.rm_f('out/circles.png')
-    F { File.exist? 'out/circles.png' }
-    (0..30).each do |d|
-      centre = Point[sqrt(d), sqrt(d)]
-      radius = sqrt(2.2*d)
-      circle(:centre => centre, :radius => radius)
-    end
-    render("out/circles.png")
-    T { File.exist? 'out/circles.png' }
+  def wrap_test(name)
+    filename = "out/#{name}.png"
+    F { File.exist? filename }
+    yield
+    render filename
+    T { File.exist? filename }
   end
 
-  NTRIANGLES = 50
+  D "circles" do
+    wrap_test "circles" do
+      (0..30).each do |d|
+        centre = Point[sqrt(d), sqrt(d)]
+        radius = sqrt(2.2*d)
+        circle(:centre => centre, :radius => radius)
+      end
+    end
+  end
+
+  NTRIANGLES = 10
 
   D "triangle spiral (#{NTRIANGLES})" do
-    filename = "out/spiral-#{NTRIANGLES}.png"
-    FileUtils.rm_f(filename)
-    F { File.exist? filename }
-    # construction code taken from test/construct/spiral.rb
-    first = triangle(:right_angle => :first, :base => 3, :height => 1)
-    Shape.generate(NTRIANGLES, first) do |tn|
-      triangle(:base => tn.hypotenuse.reverse, :right_angle => :first, :height => 1)
+    wrap_test "spiral-#{NTRIANGLES}" do
+      # construction code taken from test/construct/spiral.rb
+      first = triangle(:right_angle => :first, :base => 3, :height => 1)
+      Shape.generate(NTRIANGLES, first) do |tn|
+        triangle(:base => tn.hypotenuse.reverse, :right_angle => :first, :height => 1)
+      end
     end
-    render(filename)
-    T { File.exist? filename }
   end
 
   D "variety" do
@@ -44,59 +53,47 @@ D "Diagrams" do
     TESTS.each do |test|
       description = test[:desc].downcase
       D "#{description}" do
-        filename = "out/variety-#{description}.png"
-        FileUtils.rm_f(filename)
-        F { File.file?(filename) }
-        RGeom::Register.instance.clear!
-        eval test[:code]
-        render filename
-        T { File.file?(filename) }
+        wrap_test "variety-#{description}" do
+          RGeom::Register.instance.clear!
+          eval test[:code]
+        end
       end
     end
   end  # variety
 
   D "polygons 1" do
     # A bunch of polygons on the same base (0-1)
-    filename = "out/polygons-1.png"
-    FileUtils.rm_f(filename)
-    F { File.file? filename }
-    b = seg(p(0,0), p(1,0))
-    (3..17).each do |n|
-      polygon(n: n, base: b)
+    wrap_test "polygons-1" do
+      b = seg(p(0,0), p(1,0))
+      (3..17).each do |n|
+        polygon(n: n, base: b)
+      end
     end
-    render filename
-    T { File.file? filename }
   end
 
   D "polygons 2" do
     # A bunch of circles containing polygons of increasing sidality
-    filename = "out/polygons-2.png"
-    FileUtils.rm_f(filename)
-    F { File.file? filename }
-    15.times do |i|
-      x = 3*(i%5)
-      y = 3*(i/5)
-      c = pt(x,y)
-      n = i+3          # triangle, square, pentagon, ...
-      circle(centre: c, radius: 1)
-      polygon(n: n, centre: c, radius: 1)
+    wrap_test "polygons-2" do
+      15.times do |i|
+        x = 3*(i%5)
+        y = 3*(i/5)
+        c = pt(x,y)
+        n = i+3          # triangle, square, pentagon, ...
+        circle(centre: c, radius: 1)
+        polygon(n: n, centre: c, radius: 1)
+      end
     end
-    render filename
-    T { File.file? filename }
   end
 
   D "polygons 3" do
     # A miscellaneous triangle with a different polygon on each side
-    filename = "out/polygons-3.png"
-    FileUtils.rm_f(filename)
-    F { File.file? filename }
-    points A: p(5.7,1.9), B: p(-4.5,-4.189)
-    triangle(:ABC, :equilateral)
-    polygon n: 7, base: :BA
-    polygon n: 5, base: :CB
-    polygon n:10, base: :AC
-    render filename
-    T { File.file? filename }
+    wrap_test "polygons-3" do
+      points A: p(5.7,1.9), B: p(-4.5,-4.189)
+      triangle(:ABC, :equilateral)
+      polygon n: 7, base: :BA
+      polygon n: 5, base: :CB
+      polygon n:10, base: :AC
+    end
   end
 
 end if defined? Cairo
